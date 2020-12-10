e8586dab6c3fefff1304575fe4ef8dead0deca51
Marius <mariusdieckmann90@gmail.com>
2020-12-10 14:11:40 +0100

    fixed wrong tmp dir parameter in bakta command

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ mkdir /output
 echo $DownloaderEnvConfig
 
 eval "/bin/DataStager $DownloaderEnvConfig"
-/entrypoint.sh "$BaktaEnvConfig" -o /output --tmp_dir /cache -t 4
+/entrypoint.sh "$BaktaEnvConfig" -o /output --tmp-dir /cache -t 4
 tar -czvf results.tar.gz /output
 eval "/bin/DataStager $UploaderEnvConfig"
